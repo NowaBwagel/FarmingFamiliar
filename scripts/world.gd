00afb8da6fc4912774b0a7ae59402427b2a632c3
DESKTOP-GFQ8TGE\noahb <noah.bergl@gmail.com>
2024-03-12 17:38:27 -0500

    world start

--- scripts/world.gd
+++ scripts/world.gd
@@ -10,9 +10,11 @@ extends Node2D
 func _ready():
 	tile_map.clear()
 	var update_cells: Array[Vector2i] = []
-	for x in range(128):
-		for y in range(128):
-			if noise.get_noise_2d(x, y) >= 0.3:
+	for x in range(-32,32):
+		for y in range(-32,32):
+			var value: float = (((x) * (x))/((24.0 * 24.0)) + ((y)*(y))/(16.0*16.0))
+			#print(value)
+			if value < 1.0:
 				update_cells.append(Vector2i(x,y))
 	tile_map.set_cells_terrain_connect(1, update_cells, 0, 0)
 
